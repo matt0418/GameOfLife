d4abb3ead2e0db03be2a9e37dd65043d7ac11384
matt0418 <mfeldma7@gmail.com>
2019-07-16 18:14:10 -0400

    trying to understand better

--- game-of-life/src/gameComponents/Cell.js
+++ game-of-life/src/gameComponents/Cell.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-class Cell extends React.Component {
-    render() {
-      return (
-        <div onClick={() => this.props.storeCell(this.props.position)} className={this.props.live ? "cellContainerLive" : "cellContainerDead"}></div>
-      );
-    }
+const Cell = props => {
+    return (
+        <div onClick={() => props.storeCell(props.position)} className={props.live ? "cellContainerLive" : "cellContainerDead"}></div>
+    );
 }
 
 export default Cell
--- game-of-life/src/gameComponents/Game.js
+++ game-of-life/src/gameComponents/Game.js
@@ -13,6 +13,19 @@ class Game extends React.Component {
         }
     }
 
+    handleClear = () => {
+        this.setState({
+            ...this.state,
+            universe: new Universe(),
+            gameRunning: false,
+            interval: 100
+        }, () => {
+            if (this.intervalRef) {
+                clearInterval(this.intervalRef)
+            }
+        })
+    }
+
     handleRowChange = e => {
         if (!this.state.gameRunning) {
             let actualSize = this.state.size
@@ -95,10 +108,10 @@ class Game extends React.Component {
             cellRow=[]
         }
         return newWorld
-
     }
 
     render() {
+        console.log(this.state.universe)
         return (
             <div className="worldContainer">
               <div className="headerContainer">
@@ -115,6 +128,7 @@ class Game extends React.Component {
                 <div className="headerButtons">
                   <button className="submit" onClick={this.startGame}>Start</button>
                   <button className="submit" onClick={this.stopGame}>Stop</button>
+                  <button className="submit" onClick={this.handleClear}>Clear</button>
                 </div>
                 Generation: {this.state.universe.getGeneration()}
               </div>
@@ -127,151 +141,3 @@ class Game extends React.Component {
 }
 
 export default Game
-
-// export default class Game extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         universe: new Universe(),
-//         size: [90, 20],
-//         gameRunning: false,
-//         interval: 100
-//       }
-  
-//       this.handleColumnChange = this.handleColumnChange.bind(this);
-//       this.handleRowChange = this.handleRowChange.bind(this);
-//       this.startGame = this.startGame.bind(this);
-//       this.stopGame = this.stopGame.bind(this);
-//       this.renderBoard = this.renderBoard.bind(this);
-//       this.storeCell = this.storeCell.bind(this);
-//     }
-  
-//     handleRowChange(event) {
-//       if(!this.state.gameRunning) {
-//         var actualSize = this.state.size;
-  
-//         if(event.target.value < 20)
-//           actualSize[1] = event.target.value;
-//         else
-//           actualSize[1] = 20;
-  
-//         this.setState({
-//           size: actualSize,
-//         });
-  
-//         this.renderBoard();
-//       }
-//     }
-  
-//     handleColumnChange(event) {
-//       if(!this.state.gameRunning) {
-//         var actualSize = this.state.size;
-//         if(event.target.value < 90)
-//           actualSize[0] = event.target.value;
-//         else
-//           actualSize[0] = 90;
-  
-//         this.setState({
-//           size: actualSize,
-//         });
-  
-//         this.renderBoard();
-//       }
-//     }
-  
-//     changeInterval = (event) => {
-//       if(!this.state.gameRunning){
-//         this.setState({
-//           interval: event.target.value
-//         })
-//       }
-//     }
-  
-//     startGame() {
-//       if(!this.state.gameRunning){
-//         this.setState({
-//           gameRunning: true,
-//         }, () => {
-//           this.intervalRef = setInterval(() => this.runGame(), this.state.interval);
-//         })
-//       }
-//     }
-  
-//     stopGame(){
-//       this.setState({
-//         gameRunning: false
-//       }, () => {
-//         if(this.intervalRef) {
-//           clearInterval(this.intervalRef);
-//         }
-//       })
-//     }
-  
-//     runGame() {
-//       this.setState({
-//         universe: this.state.universe.addGeneration()
-//       })
-//     }
-  
-//     storeCell(position) {
-//       if(!this.state.gameRunning) {
-//         this.setState({
-//           universe: this.state.universe.storeCell(position)
-//         })
-//       }
-//     }
-  
-//     renderBoard() {
-//       var newWorld = [];
-//       var cellRow = [];
-  
-//       for(var i = 0; i < this.state.size[0]; i++) {
-//         for (var j = 0; j < this.state.size[1]; j++){
-//           if(this.state.universe.isCellAlive(i + " , " + j)){
-//             cellRow.push(
-//               <Cell key={[i, j]} position={{x: i, y: j}} live={true} storeCell={this.storeCell.bind(this)}/>
-//             );
-//           } else {
-//             cellRow.push(
-//               <Cell key={[i, j]} position={{x: i, y: j}} live={false} storeCell={this.storeCell.bind(this)}/>
-//             );
-//           }
-//         }
-//         newWorld.push(<div className="row" key={i}>{cellRow}</div>);
-//         cellRow = [];
-//       }
-  
-//       return newWorld;
-//     }
-  
-//     render() {
-//       return (
-//         <div className="worldContainer">
-//           <div className="headerContainer">
-//             <div className="headerInnerContainer">
-//               <label className="label">
-//                 Rows:
-//                 <input className="input" type="text" value={this.state.size[1]} onChange={this.handleRowChange} />
-//               </label>
-//               <label className="label">
-//                 Columns:
-//                 <input className="input" type="text" value={this.state.size[0]} onChange={this.handleColumnChange} />
-//               </label>
-//               <label className="label">
-//                 Interval:
-//                 <input className="input" type="text" value={this.state.interval} onChange={this.changeInterval} />
-//               </label>
-//             </div>
-//             <div className="headerButtons">
-//               <button className="submit" onClick={this.startGame}>Start</button>
-//               <button className="submit" onClick={this.stopGame}>Stop</button>
-//             </div>
-//             Generation: {this.state.universe.getGeneration()}
-//           </div>
-//           <div className="boardContainer">
-//           {this.renderBoard()}
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
